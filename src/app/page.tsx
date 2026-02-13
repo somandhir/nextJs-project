0b7703b497ce88a9995d2ba05d4e1858d4c6b8f7
Soman <somandhir@gmail.com>
2026-02-13 17:17:47 +0530

    feat: add new components and styles for music courses landing page
    - Created Navbar component for navigation.
    - Developed HeroSection component for the landing page with spotlight effect.
    - Implemented moving border button component for course exploration.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import HeroSection from "@/components/HeroSection";
+
 export default function Home() {
-  return <h1 className="text-2xl text-center">learning next js...</h1>;
+  return (
+    <main className="pt-32 min-h-screen bg-black/96 antialiased bg-grid-white/[0.02] ">
+      <HeroSection />
+    </main>
+  );
 }
